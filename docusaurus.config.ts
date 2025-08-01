90d7a486a7e3883fa2d0be97197c125a5aab89e8
ferch <fanchunhua116@gmail.com>
2025-08-01 12:30:49 +0800

    add google gtag

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,8 +58,27 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.scss',
         },
+        debug: true,
+        sitemap: {
+          lastmod: 'date',
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+          createSitemapItems: async (params) => {
+            const {defaultCreateSitemapItems, ...rest} = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => !item.url.includes('/page/'));
+          },
+        },
+        gtag: {
+          trackingID: 'G-K8KNXCXPT9',
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
+    
+
   ],
   themeConfig: {
     prism: {
